d366e45e5a65ccac4f868301998a435bcd7792c2
ACrazyTown <47027981+ACrazyTown@users.noreply.github.com>
2025-02-03 11:19:21 +0100

    Undefine EGL

--- project/lib/custom/sdl/include/SDL_config_macosx.h
+++ project/lib/custom/sdl/include/SDL_config_macosx.h
@@ -210,9 +210,9 @@
 #ifndef SDL_VIDEO_OPENGL_ES2
 #define SDL_VIDEO_OPENGL_ES2    1
 #endif
-#ifndef SDL_VIDEO_OPENGL_EGL
-#define SDL_VIDEO_OPENGL_EGL    1
-#endif
+// #ifndef SDL_VIDEO_OPENGL_EGL
+// #define SDL_VIDEO_OPENGL_EGL    1
+// #endif
 #ifndef SDL_VIDEO_OPENGL_CGL
 #define SDL_VIDEO_OPENGL_CGL    1
 #endif
